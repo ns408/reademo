bb662084a001f6e4ecfe35a3b02322d95545770d
Liam <lnunns86@gmail.com>
2017-01-12 23:47:48 +1100

    commit of service resource

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,9 +3,11 @@ class reademo::install (
 {
   package { 'passenger':
      ensure   => present,
-    }
+    } -> # and then:
   package { 'nginx':
           ensure  => present,
-        }
-
+        } -> # and then:
+ service { 'nginx':
+          enable  => true,
+          ensure  => running,
 }
