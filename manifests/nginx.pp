33f0d697b94d7d4b260f8aa5e15fa73a249b86c5
Liam <lnunns86@gmail.com>
2017-01-12 23:58:11 +1100

    accidentally forgot to hit save on nginx removal

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,13 +1,6 @@
 class reademo::nginx (
 )
 {
-file { '/etc/nginx/conf.d/passenger.conf':
-  ensure  => present,
-  content => template('reademo/nginxpassenger.erb'),
-  owner   => 'nginx',
-  group   => 'nginx',
-  mode    => '0644',
-  notify  => Service['nginx'],
-}
+
 
 }
