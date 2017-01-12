6045f3c9fc496de82d66c5df778df1c7769186a1
Liam <lnunns86@gmail.com>
2017-01-12 23:37:06 +1100

    commit of init logic

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,10 @@
 class reademo (
+$ruby_version = 2.2.4,
 )
 {
-class { 'ruby':
-  gems_version => '2.2.4',
-}
+
+class { '::reademo::repo': } -> # and then:
+class { '::reademo::ruby': } -> # and then:
+class { '::reademo::install': } -> # and then:
+class { '::reademo::nginx': }
 }
--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,12 +1,4 @@
 class reademo::packages (
 )
 {
-
-
-  package { 'passenger':
-     ensure   => present,
-    }
-  package { 'nginx':
-          ensure  => present,
-        }
 }
