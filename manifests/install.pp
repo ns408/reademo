384730b0cef57f8d6bd4ae4151b7e19fa5a66ce1
Liam <lnunns86@gmail.com>
2017-01-12 23:57:06 +1100

    moving file from nginx to install

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,10 +6,18 @@ class reademo::install (
     }
   package { 'nginx':
           ensure  => present,
-          
-        }
+
+        } -> # and then:
  service { 'nginx':
           enable  => true,
           ensure  => running,
+        } -> # and then:
+file { '/etc/nginx/conf.d/passenger.conf':
+          ensure  => present,
+          content => template('reademo/nginxpassenger.erb'),
+          owner   => 'nginx',
+          group   => 'nginx',
+          mode    => '0644',
+          notify  => Service['nginx'],
         }
 }
