b1f31e100157d4d0fed39f0b0fa99f94781de72d
Nunns, Liam ADM account <adm-e1069201@fnfis.com>
2017-01-15 22:17:23 +1100

    commit of management of nginx.conf

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,46 +1,55 @@
 class reademo::install (
-)
-inherits reademo
-{
-  package { 'passenger':
-     ensure   => present,
-    }
-    group { "web":
-          ensure  => present,
-        }
-    user { "nginx":
-          ensure   => present,
-          groups    => 'web',
-         }
-  package { 'nginx':
-          ensure  => present,
-          require => User['nginx'],
-        }
+  )
+  inherits reademo
+  {
+    package { 'passenger':
+    ensure   => present,
+  }
+  group { "web":
+  ensure  => present,
+}
+user { "nginx":
+ensure   => present,
+groups    => 'web',
+}
+package { 'nginx':
+ensure  => present,
+require => User['nginx'],
+}
 file { '/etc/nginx/conf.d/passenger.conf':
-          ensure  => present,
-          content => template('reademo/nginxpassenger.erb'),
-          owner   => 'nginx',
-          group   => 'nginx',
-          mode    => '0644',
-          notify  => Service['nginx'],
-          require => Package['nginx'],
-        }
+ensure  => present,
+content => template('reademo/nginxpassenger.erb'),
+owner   => 'nginx',
+group   => 'nginx',
+mode    => '0644',
+notify  => Service['nginx'],
+require => Package['nginx'],
+}
 file { '/etc/nginx/conf.d/rea.conf':
-          ensure  => present,
-          content => template('reademo/rea.erb'),
-          owner   => 'nginx',
-          group   => 'nginx',
-          mode    => '0644',
-          notify  => Service['nginx'],
-          require => Package['nginx'],
-        }
+ensure  => present,
+content => template('reademo/rea.erb'),
+owner   => 'nginx',
+group   => 'nginx',
+mode    => '0644',
+notify  => Service['nginx'],
+require => Package['nginx'],
+}
 file { '/etc/nginx/conf.d/buffer.conf':
-          ensure  => present,
-          content => template('reademo/buffer.erb'),
-          owner   => 'nginx',
-          group   => 'nginx',
-          mode    => '0644',
-          notify  => Service['nginx'],
-          require => Package['nginx'],
-                }
+ensure  => present,
+content => template('reademo/buffer.erb'),
+owner   => 'nginx',
+group   => 'nginx',
+mode    => '0644',
+notify  => Service['nginx'],
+require => Package['nginx'],
+}
+file { '/etc/nginx/conf.d/nginx.conf':
+ensure  => present,
+content => template('reademo/nginx.erb'),
+owner   => 'nginx',
+group   => 'nginx',
+mode    => '0644',
+notify  => Service['nginx'],
+require => Package['nginx'],
+}
 }
