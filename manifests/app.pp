ae0fc5a1bdfcb74cc3252dfc2af6561213fe9440
Nunns, Liam ADM account <adm-e1069201@fnfis.com>
2017-01-15 17:09:05 +1100

    adding nginx as member of web

--- manifests/app.pp
+++ manifests/app.pp
@@ -3,6 +3,7 @@ class reademo::app (
 {
   group {"web":
         ensure  => present,
+        members => 'nginx',
         before => User['rea'],
       }
   user { "rea":
@@ -18,8 +19,10 @@ class reademo::app (
   file { "/var/www/rea":
     ensure => directory,
     owner => 'rea',
-    mode  => '0700',
+    group => 'web',
+    mode  => '0750',
   }
+
   exec { "/bin/git clone https://github.com/rea-cruitment/simple-sinatra-app.git ./":
   cwd     => '/var/www/rea/',
   creates => '/var/www/rea/config.ru',
